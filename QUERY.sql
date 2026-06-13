c0887ab4ea233b7a56dfc9e7990451f56973f321
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-06-14 01:02:31 +0600

    added third query problem solution

--- QUERY.sql
+++ QUERY.sql
@@ -86,3 +86,7 @@ WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 SELECT user_id, full_name, email FROM users
 WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
 
+-- Query 3:
+SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status FROM bookings
+WHERE payment_status IS NULL;
+
