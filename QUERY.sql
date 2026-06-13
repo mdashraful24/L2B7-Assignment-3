dd0d21d4381f482315fe374271ebd8f3901b9c7e
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-06-14 03:39:27 +0600

    added updated query four and five query structure

--- QUERY.sql
+++ QUERY.sql
@@ -79,35 +79,37 @@ SELECT * FROM Bookings;
 -- =========================
 
 -- Query 1:
-SELECT match_id, fixture, ROUND(base_ticket_price) AS base_ticket_price FROM matches
+SELECT match_id, fixture, ROUND(base_ticket_price) AS base_ticket_price FROM Matches
 WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 
 -- Query 2:
-SELECT user_id, full_name, email FROM users
+SELECT user_id, full_name, email FROM Users
 WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
 
 -- Query 3:
-SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status FROM bookings
+SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status FROM Bookings
 WHERE payment_status IS NULL;
 
 -- Query 4:
-SELECT booking_id, full_name, fixture, ROUND(total_cost) AS total_cost FROM bookings
-INNER JOIN users USING (user_id)
-INNER JOIN matches USING (match_id);
+SELECT b.booking_id, u.full_name, m.fixture, ROUND(b.total_cost) AS total_cost FROM Bookings AS b
+INNER JOIN Users AS u ON b.user_id = u.user_id
+INNER JOIN Matches AS m ON b.match_id = m.match_id;
 
 -- Query 5:
-SELECT user_id, full_name, booking_id FROM users
-LEFT JOIN bookings USING (user_id);
+SELECT u.user_id, u.full_name, b.booking_id FROM Users AS u
+LEFT JOIN Bookings AS b ON u.user_id = b.user_id;
 
 -- Query 6:
-SELECT booking_id, match_id, ROUND(total_cost) AS total_cost FROM bookings
+SELECT booking_id, match_id, ROUND(total_cost) AS total_cost FROM Bookings
 WHERE total_cost > (
-  SELECT AVG(total_cost) FROM bookings
+  SELECT AVG(total_cost) FROM Bookings
 );
 
 -- Query 7:
-SELECT match_id, fixture, ROUND(base_ticket_price) AS base_ticket_price FROM matches
+SELECT match_id, fixture, ROUND(base_ticket_price) AS base_ticket_price FROM Matches
 WHERE base_ticket_price < (
-  SELECT MAX(base_ticket_price) FROM matches
-) ORDER BY base_ticket_price DESC LIMIT 2;
+  SELECT MAX(base_ticket_price) FROM Matches
+)
+ORDER BY base_ticket_price DESC
+LIMIT 2;
 
